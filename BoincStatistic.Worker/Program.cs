b31aa120b1ee7b5b68354b611bbcf61c4fc57bcc
Alexander <vsany@meta.ua>
2024-10-29 13:27:12 +0200

    small fix

--- BoincStatistic.Worker/Program.cs
+++ BoincStatistic.Worker/Program.cs
@@ -1,13 +1,10 @@
 using System;
 using BoincStatistic.Database;
-using BoincStatistic.Database.BoincProjectStats;
 using BoincStatistic.Worker;
 using Microsoft.EntityFrameworkCore;
-// using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration(
