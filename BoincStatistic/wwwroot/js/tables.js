6958b13b4d8e38c6851c89441236566f5c6e50d2
Alexander <vsany@meta.ua>
2025-03-04 12:31:26 +0200

    stage. Implemented Tailwind styles for tables

---
+++ BoincStatistic/wwwroot/js/tables.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+    $('#newsTable').DataTable({
+        "order": [],
+        "paging": false,
+        "info": false,
+        "columnDefs": [
+            { "orderable": true, "targets": "_all" },
+            {
+                "type": "html",
+                "targets": 0
+            },
+            {
+                "type": "num",
+                "targets": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+            },
+            {
+                "orderable": false,
+                "targets": [13]
+            }
+        ],
+        "language": {
+            "search": "Filter:"
+        }
+    });
+});
